b605a0275684db9f67309bde97e45ddf746b5f7e
Abass Bayo <abass@ootp.io>
2022-12-25 04:55:34 -0500

    Trying to get iOS app to build with swiftUi views

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -3,13 +3,9 @@ import shared
 
 @main
 struct iOSApp: App {
-    private let databaseModule = DatabaseModule()
-
 	var body: some Scene {
-    		WindowGroup {
-                NavigationView {
-                    NoteListScreen(noteDataSource: databaseModule.noteDataSource)
-                }.accentColor(.black)
-    		}
-    	}
+		WindowGroup {
+			ContentView()
+		}
+	}
 }
