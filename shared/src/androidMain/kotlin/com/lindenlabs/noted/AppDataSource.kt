a786f5c3f357b87aa0450a6a39418e1d6941f1f3
Abass Bayo <abass@ootp.io>
2022-12-25 02:52:33 -0500

    Add core elements for relevant modules

---
+++ shared/src/androidMain/kotlin/com/lindenlabs/noted/AppDataSource.kt
@@ -0,0 +1,39 @@
+package com.lindenlabs.noted
+
+import com.lindenlabs.noted.data.Note
+import com.lindenlabs.noted.data.NoteDataSource
+import com.lindenlabs.noted.database.NoteDatabase
+import com.lindenlabs.noted.utils.DateTimeUtil
+
+class AppDataSource(db: NoteDatabase): NoteDataSource {
+
+    private val queries = db.noteQueries
+
+    override suspend fun insertNote(note: Note) {
+        queries.insertNote(
+            id = note.id,
+            title = note.title,
+            content = note.content,
+            colorHex = note.colorHex,
+            created = DateTimeUtil.toEpochMillis(note.created)
+        )
+    }
+
+    override suspend fun getNoteById(id: Long): Note? {
+        return queries
+            .getNoteById(id)
+            .executeAsOneOrNull()
+            ?.let { Note.fromNoteEntity(it) }
+    }
+
+    override suspend fun getAllNotes(): List<Note> {
+        return queries
+            .getAllNotes()
+            .executeAsList()
+            .map { Note.fromNoteEntity(it) }
+    }
+
+    override suspend fun deleteNoteById(id: Long) {
+        queries.deleteNoteById(id)
+    }
+}
