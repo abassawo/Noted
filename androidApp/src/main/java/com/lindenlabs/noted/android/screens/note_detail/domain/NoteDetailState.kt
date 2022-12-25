6e8e44b3c9ee4094d1cda02330dd369d07fad2cf
Abass Bayo <abass@ootp.io>
2022-12-25 03:53:09 -0500

    Started detail screen ui

---
+++ androidApp/src/main/java/com/lindenlabs/noted/android/screens/note_detail/domain/NoteDetailState.kt
@@ -0,0 +1,9 @@
+package com.lindenlabs.noted.android.screens.note_detail.domain
+
+data class NoteDetailState(
+    val noteTitle: String = "",
+    val isNoteTitleHintVisible: Boolean = false,
+    val noteContent: String = "",
+    val isNoteContentHintVisible: Boolean = false,
+    val noteColor: Long = 0xFFFFFF
+)
