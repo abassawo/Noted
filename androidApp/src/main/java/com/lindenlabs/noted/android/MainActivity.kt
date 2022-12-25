ae970a1b7a9e0913410f2747065b4909f0ad5b40
Abass Bayo <abass@ootp.io>
2022-12-25 03:59:09 -0500

    Android functionality done

--- androidApp/src/main/java/com/lindenlabs/noted/android/MainActivity.kt
+++ androidApp/src/main/java/com/lindenlabs/noted/android/MainActivity.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.lindenlabs.noted.android.screens.note_detail.views.NoteDetailView
 import com.lindenlabs.noted.android.screens.show_notes.views.NoteListScreen
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -27,10 +32,22 @@ class MainActivity : ComponentActivity() {
                     composable(route = "note_list") {
                         NoteListScreen(navController = navController)
                     }
+                    composable(
+                        route = "note_detail/{noteId}",
+                        arguments = listOf(
+                            navArgument(name = "noteId") {
+                                type = NavType.LongType
+                                defaultValue = -1L
+                            }
+                        )
+                    ) { backStackEntry ->
+                        val noteId = backStackEntry.toNoteId()
+                        NoteDetailView(noteId = noteId, navController = navController)
+                    }
                 }
             }
         }
+
+        private fun NavBackStackEntry.toNoteId(): Long = arguments?.getLong("noteId") ?: -1L
     }
-
 }
-
